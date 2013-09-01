9e3c5aeb0d86b0b63bdead702a2830dc92005038
albertnez <albertmg7@gmail.com>
2013-09-01 17:49:50 +0200

    path is filled in 3d now, still some bug to fix

--- cube.js
+++ cube.js
@@ -16,56 +16,55 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 
-
+// BOARD PROPERTIES
 var numW = 10; // number of tiles wide
 var numH = 10; // number of tiles high
+var tileWidth = 1; // how many wide 
+var tileHeight = 1; // how many tall 
 
+//Materials with basic colors
 var materialWire  = new THREE.MeshBasicMaterial({color:0x000000, wireframe:true}); // matIdx = 0
+var materialInvisible = new THREE.MeshBasicMaterial({visible:false});
+var materialBlack = new THREE.MeshBasicMaterial({color:0x000000});
+var materialWhite = new THREE.MeshBasicMaterial({color:0xFFFFFF});
 var materialRed   = new THREE.MeshBasicMaterial({color:0xFF0000}); 
 var materialGreen = new THREE.MeshBasicMaterial({color:0x00FF00}); 
 
-// One material per tile
+// Each tile has a material assigned to it.
 var materialsArray = [numH*numW];
 for (var i = numW-1; i >= 0; --i) {
 	for (var j = numH-1; j >= 0; --j) {
-		materialsArray[i+j*numW] = materialWire;
+		materialsArray[i+j*numW] = ((i+j)%2 ? materialBlack : materialWhite);
 	}
 }
 var multiMaterial = new THREE.MeshFaceMaterial( materialsArray );
 
-var planeWidth = 1; // how many wide 
-var planeHeight = 1; // how many tall 
-var planeGeo = new THREE.PlaneGeometry( numW*planeWidth, numH*planeHeight, numW, numH );
+var planeGeo = new THREE.PlaneGeometry( numW*tileWidth, numH*tileHeight, numW, numH );
 for (var i = 0; 2*i < numW*numH*2; ++i) {
 	planeGeo.faces[i*2].materialIndex = planeGeo.faces[i*2+1].materialIndex = i;
 }
-
-
 var plane = new THREE.Mesh( 
-	//new THREE.PlaneGeometry( numW*planeWidth, numH*planeHeight, numW, numH ), 
-	//new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true } ) 
 	planeGeo,
 	multiMaterial
 );
-plane.position.x = numW*planeWidth*0.5-0.5;
-plane.position.y = -numH*planeHeight*0.5+0.5;
+//make the top left tile be at (0,0);
+plane.position.x = numW*tileWidth*0.5-0.5; 
+plane.position.y = -numH*tileHeight*0.5+0.5;
 scene.add(plane);
-
-
 
 
 function Player(_color) {
 	this.c = _color;
-	this.cube = new THREE.Mesh( new THREE.CubeGeometry(1,1,1), new THREE.MeshLambertMaterial( { color: (_color  ? _color :0xff0000) } ) );
+	this.cube = new THREE.Mesh(
+		new THREE.CubeGeometry(1,1,1),
+		new THREE.MeshLambertMaterial( { color: (_color  ? _color :0xff0000) })
+	);
 	this.cube.position.z = 0.5; // avoid being inside surface
 	scene.add(this.cube);
 	this.direction = -1;
 	this.speed = 2;
 
-	this.logic = function() {
-		//plane.geometry.faces[(this.cube.position.x)*2+(this.cube.position.y*-1*2*numW)].materialIndex = 1;
-		materialsArray[this.cube.position.x+this.cube.position.y*-1*numW] = (this.c === 'green' ? materialGreen : materialRed);
-
+	this.update = function() {
 		switch(this.direction) {
 			case 0: 
 				if (this.cube.position.x < numW-1) ++this.cube.position.x; 
@@ -84,6 +83,7 @@ function Player(_color) {
 				else this.direction = -1;
 			break;
 		}
+		materialsArray[this.cube.position.x+this.cube.position.y*-1*numW] = (this.c === 'green' ? materialGreen : materialRed);
 	}
 
 	this.control = function(dt) {
@@ -96,9 +96,9 @@ function Player(_color) {
 }
 
 var player = new Player('red');
-
 //Oponent for testing
 var op = new Player('green');
+
 op.control = function(dt) { //for testing purposes
 		if (kb.char('I')) this.direction = 1;
 		if (kb.char('J')) this.direction = 2;
@@ -110,6 +110,7 @@ op.cube.position.x = 2;
 scene.add(op.cube)
 
 function cameraLogic(dt, p) {
+	if (p === undefined) p = player;
 	var speed = SPS/2; // CHECK THIS
 	var dist = CAMERA_DISTANCE;
 	camera.position.x += (p.cube.position.x - camera.position.x)*dt*speed;
@@ -128,15 +129,26 @@ camera.position.z = 10; // distance from floor
 camera.position.x = camera.position.y = 5000; // inital effect
 camera.rotation.x = 0.7;
 
-
-function logic(dt) {
-	player.logic(dt);
-	if (kb.char('Z')) camera.position.z += dt*5;
-	if (kb.char('X')) camera.position.z -= dt*5;
-	if (kb.char('R')) camera.rotation.x += dt;
-	if (kb.char('T')) camera.rotation.x -= dt;
+var players = [player, op];
+var map = []; //actual map
+for (var i = 0; i < numH*numW; ++i) {
+	map[i] = new THREE.Mesh( new THREE.CubeGeometry(1,1,0.5), materialInvisible	);
+	map[i].position.z = 0.25;
+	map[i].position.x = i%numW;
+	map[i].position.y = -Math.floor(i/numW);
+	scene.add(map[i]);
 }
 
+
+function update(dt) {
+	for (var i = players.length-1; i >= 0; --i) {
+		map[players[i].cube.position.x+players[i].cube.position.y*-1*numW].material = new THREE.MeshBasicMaterial({color:players[i].c});
+		players[i].update(dt);
+	}
+}
+function control() {
+	for (var i = players.length-1; i >= 0; --i) players[i].control();
+}
 
 var focusPlayer = true;
 var focusChanged = false;
@@ -147,13 +159,12 @@ function render() {
 	var dt = (time-oldTime)/1000;
 	oldTime = time;
 
-	if (time-oldLogic > 1000/SPS) {
-		logic(dt);
-		op.logic(dt);
-		oldLogic = time;
+	if (time-oldUpdate > 1000/SPS) {
+		update(dt);
+		oldUpdate = time;
 	}
-	else {
-	}
+
+	control();
 
 	if (kb.char('B') && !focusChanged) {
 		focusPlayer = !focusPlayer;
@@ -161,8 +172,11 @@ function render() {
 	}
 	else if (!kb.char('B')) focusChanged = false;
 
-	op.control();
-	player.control();
+	//CAMERA STUFF
+	if (kb.char('Z')) camera.position.z += dt*5;
+	if (kb.char('X')) camera.position.z -= dt*5;
+	if (kb.char('R')) camera.rotation.x += dt;
+	if (kb.char('T')) camera.rotation.x -= dt;
 	
 	if (focusPlayer) cameraLogic(dt, player);
 	else cameraLogic(dt, op);
@@ -171,5 +185,5 @@ function render() {
 }
 
 var oldTime = new Date().getTime();
-var oldLogic = 0;
+var oldUpdate = 0;
 render();
